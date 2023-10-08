79566132ef06f69a969a1617a05bb9c216a884b2
Binh Nguyen (Irene) <98362650+irenenguyen1017@users.noreply.github.com>
2023-10-08 22:29:01 +1100

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+
+
+
 export default function App() {
   return (
-    <button className="btn btn-secondary">Secondary</button>
+    <Routes>
+      <Route path='/' element= { <Home />} />
+      <Route path='/movie/:movieId' element= { <Movie />}/>
+    </Routes>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <p>This is the Home page</p>
+        </div>
+    )
+}
+
+export default Home;
